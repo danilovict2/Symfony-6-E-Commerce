b285f64a4afeb717a73697798bddd5ac7cfcddb9
danilovict2 <danilosindric@gmail.com>
2023-07-25 18:30:52 +0200

    Added product index pagea

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
