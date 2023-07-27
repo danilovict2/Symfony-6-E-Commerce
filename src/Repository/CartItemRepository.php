1604fa3c9ed90d90b7e3b15ef3001e5a98abbfd4
danilovict2 <danilosindric@gmail.com>
2023-07-27 18:52:33 +0200

    Prepare helper methods for cart

--- src/Repository/CartItemRepository.php
+++ src/Repository/CartItemRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\CartItem;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -19,6 +21,29 @@ class CartItemRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, CartItem::class);
+    }
+
+    public function getUserCartItemsCount(User $user): int
+    {
+        return $this->getUserCartItemsQuery($user)->select('SUM(quantity)')->getQuery()->execute();
+    }
+
+    public function getUserCartItems(User $user): array
+    {
+        return $this->getUserCartItemsQuery($user)->select('product, quantity')->getQuery()->execute();
+    }
+
+    public function getUserCartItemsGroupedByProduct(User $user): array
+    {
+        return $this->getUserCartItemsQuery($user)->select('product, quantity')->groupBy('product')->getQuery()->execute();
+    }
+
+    private function getUserCartItemsQuery(User $user): QueryBuilder
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.user = :user')
+            ->setParameter('user', $user)
+        ;
     }
 
     public function save(CartItem $entity, bool $flush = false): void
